d1b25b1cec2d30b344c96a9c463ddb1a8011f884
Seungsi <117188786+Seungsi@users.noreply.github.com>
2024-06-20 12:16:17 +0000

    30 끝

--- lesson-30/app.js
+++ lesson-30/app.js
@@ -96,20 +96,17 @@ app.use((req, res, next) => {
  */
 
 // 애플리케이션에 Mongoose 설정
-const mongoose = require("mongoose"), // mongoose를 요청
-  dbName = "aaronkr";
+const mongoose = require("mongoose"); // mongoose를 요청
+
 
 // 데이터베이스 연결 설정
-mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
-  useNewUrlParser: true,
-});
-
-// 연결되면 메시지를 보냄
+mongoose.connect(
+  "mongodb+srv://seungsi:tmdrlqlqjs0911@ut-node.8n0krvz.mongodb.net/?retryWrites=true&w=majority&appName=ut-node" //Atlas경로
+);
 const db = mongoose.connection;
-db.once("open", () => {
-  console.log(`Connected to ${dbName} MongoDB using Mongoose!`);
+db.once("open",() => {
+  console.log("Connected to MONGODB!!!");
 });
-
 /**
  * =====================================================================
  * Define app settings and middleware
@@ -173,5 +170,9 @@ const server = app.listen(app.get("port"), () => {
   // 3000번 포트로 리스닝 설정
   console.log(`Server running at http://localhost:${app.get("port")}`);
 });
+const io = require("socket.io")(server);// Lesson 30 - socket.io를 사용하기 위한 서버 객체 설정
+require("./controllers/chatController")(io);// Lesson 30 - 채팅 컨트롤러를 요청하고 서버 객체를 전달
+
+
 // Lesson 30 - socket.io를 사용하기 위한 서버 객체 설정
 // Lesson 30 - 채팅 컨트롤러를 요청하고 서버 객체를 전달
